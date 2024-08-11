774dfb7a0f7963d43c61023dc7801006fed1ae4c
hoangkhoi2003 <hoangkhoitl2003@gmail.com>
2024-08-11 22:54:39 +0700

    BuiHoangKhoi: finish Chuong4 Bai 16 Trang 30

---
+++ KTLT/Chuong4/BTChuong4/BTChuong4/Bai16_Trang30.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+
+long long tinhXn_DeQuy(int n) {
+    if (n == 1 || n == 2) return 1;
+    return tinhXn_DeQuy(n - 1) + (n - 1) * tinhXn_DeQuy(n - 2);
+}
+
+
+long long tinhXn_KhuDeQuy(int n) {
+    if (n == 1 || n == 2) return 1;
+
+    long long x1 = 1, x2 = 1, xn;
+    for (int i = 3; i <= n; i++) {
+        xn = x2 + (i - 1) * x1;
+        x1 = x2;
+        x2 = xn;
+    }
+    return xn;
+}
+
+int main() {
+    int n;
+    printf("Nhap gia tri n >= 1): ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("n phai lon hon hoac bang 1.\n");
+        return 1;
+    }
+
+    printf("X_%d (de quy) = %lld\n", n, tinhXn_DeQuy(n));
+    printf("X_%d (khu de quy) = %lld\n", n, tinhXn_KhuDeQuy(n));
+
+    return 0;
+}
